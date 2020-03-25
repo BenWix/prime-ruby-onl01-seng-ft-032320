a4bb0c805352d9a66e2865c7b45841dc3e9e363b
Benjamin Wichser <benwichser@gmail.com>
2020-03-25 14:36:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(int)
-  return true if int.even?
+  return  if int.even?
   range = *(3..int/2).reject{|i| i.even?}
   remainders = []
   range.map{|i| remainders << int % i}
